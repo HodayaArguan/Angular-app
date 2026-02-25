8fec0984258fe3f05fd333bb8a4b0bfadbc6ac46
הודיה ארגואן <HodiaArguan@smg.local>
2026-02-25 17:02:50 +0200

    lesson2 commit home work of change task

--- src/app/new-lesson1/new-lesson1.component.ts
+++ src/app/new-lesson1/new-lesson1.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-lesson1',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-lesson1.component.html',
   styleUrl: './new-lesson1.component.css'
 })
-export class NewLesson1Component {
 
+
+export class NewLesson1Component {
+  taskName:string="java"
+  newTask:string=""
+  name:string="Hodaya"
+  updateTask(){
+  this.taskName=this.newTask
+  this.newTask=""
+  // flag:Boolean=true
+}
 }
